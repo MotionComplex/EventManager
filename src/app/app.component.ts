3b7f4fa5fca2aa7e2b5efbec527295cf84d875a8
Elias Douglas <eldo@NLAG086>
2018-03-26 23:22:39 +0200

    Dev: Created base styles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  loading = false;
+
+  constructor(
+    private iconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer) {
+      // register customer icons
+      this.iconRegistry.addSvgIconInNamespace('assets', 'xenocracy_white', domSanitizer.bypassSecurityTrustResourceUrl('assets/images/xenocracy_white.svg'));
+      this.iconRegistry.addSvgIconInNamespace('assets', 'xenocracy_black', domSanitizer.bypassSecurityTrustResourceUrl('assets/images/xenocracy_black.svg'));
+      this.iconRegistry.addSvgIconInNamespace('assets', 'drum_army_black', domSanitizer.bypassSecurityTrustResourceUrl('assets/images/drum_army_black.svg'));
+      this.iconRegistry.addSvgIconInNamespace('assets', 'drum_army_white', domSanitizer.bypassSecurityTrustResourceUrl('assets/images/drum_army_white.svg'));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
+// Angular imports
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MatIconModule, MatListModule, MatButtonModule, MatSidenavModule, MatSliderModule, MatProgressBarModule } from '@angular/material';
 
+// 3rd party imports
+import { CovalentLayoutModule } from '@covalent/core/layout';
+import { CovalentStepsModule  } from '@covalent/core/steps';
+import { CovalentHttpModule } from '@covalent/http';
+import { CovalentHighlightModule } from '@covalent/highlight';
+import { CovalentMarkdownModule } from '@covalent/markdown';
+import { CovalentDynamicFormsModule } from '@covalent/dynamic-forms';
 
+// Custom imports
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatProgressBarModule,
+
+    CovalentLayoutModule,
+    CovalentStepsModule,
+    CovalentHttpModule,
+    CovalentHighlightModule,
+    CovalentMarkdownModule,
+    CovalentDynamicFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
